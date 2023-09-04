4f7cc81e8593cc820d4fa1469255b91dffa6be04
ktrebor <robert.kedves.90@gmail.com>
2023-09-04 20:40:10 +0300

    Adaugat info in Db

--- Lab10Ex1/Program.cs
+++ Lab10Ex1/Program.cs
@@ -1,12 +1,47 @@
-﻿using Lab10Ex1.Models;
+﻿using Azure.Core;
+using Lab10Ex1.Models;
 
-var ionica = new Student
+Seed();
+
+static void Seed()
 {
-    Id = 1,
-    firstName = "ionica",
-    lastName = "pop",
-    age = 23,
-    major = Major.Constructii
-};
+    using var ctx = new StudentsDbContext();
 
-var ctx = new StudentsDbContext();
+    ctx.Students.Add(
+        new Student
+        {
+            firstName = "popescu",
+            lastName = "georgel",
+            age = 25,
+            major = Major.Informatica
+        });
+
+    ctx.Students.Add(
+        new Student
+        {
+            firstName = "ilinca",
+            lastName = "lacrima",
+            age = 27,
+            major = Major.Informatica
+        });
+
+    ctx.Students.Add(
+    new Student
+    {
+        firstName = "dorel",
+        lastName = "logofatu",
+        age = 23,
+        major = Major.Constructii
+    });
+
+    ctx.Students.Add(
+        new Student
+        {
+            firstName = "adriana",
+            lastName = "merlescu",
+            age = 21,
+            major = Major.Litere
+        });
+
+    ctx.SaveChanges();
+}
